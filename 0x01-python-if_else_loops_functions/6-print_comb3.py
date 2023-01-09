997ec809806720a9d5f6997e6fd322c36aac062e
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-01-09 07:39:23 -0800

    fixed error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ i = 0
 while i <= 89:
     if i % 10 == 0:
         i += 1 + i // 10
-    print("{:02d}".format(i), end='\n' if i ==89 else ", ")
-    i+= 1
+    print("{:02d}".format(i), end='\n' if i == 89 else ", ")
+    i += 1
