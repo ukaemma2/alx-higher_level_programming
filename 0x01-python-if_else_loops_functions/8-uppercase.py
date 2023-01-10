407a6595daeb99dcb2d5213792ef370fd4ca9c48
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-01-10 05:01:58 -0800

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def to_uper(char):
+    if ord(char) >= 97 and ord(char) <= 122:
+        return (ord(char) -32)
+    else:
+        return ord(char)
+
+
+def uppercase(str):
+    new = ""
+    for char in str:
+        new += "%c" % to_uper(char)
+    print("{:s}".format(new))
