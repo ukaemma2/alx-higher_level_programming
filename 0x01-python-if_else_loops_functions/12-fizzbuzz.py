003feb8515f398b3c3d3ba7cb5bdb400121f9811
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-01-10 07:10:43 -0800

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/pytbon3
+def fizzbuzz():
+    for fb in range(1, 101):
+        if fb % 15 == 0:
+            print("FizzBuzz", end=' ')
+            continue
+        elif fb % 3 == 0:
+            print("Fizz", end=' ')
+            continue
+        elif fb % 5 == 0:
+            print("Buzz", end=' ')
+        else:
+            print("{:d}".format(fb),end=' ')
+
+if __name__ == '__main__':
+    fizzbuzz()
