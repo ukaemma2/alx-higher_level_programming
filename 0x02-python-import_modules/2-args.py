41d4f4273e43432ae5be5ce73d06eb51f705be93
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-01-11 13:10:48 -0800

    error fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     elif len(argv) == 2:
         print('{:d} argument:'.format(len(argv) - 1))
     else:
-        print('{:d} argments:'.format(len(argv) - 1))
+        print('{:d} arguments:'.format(len(argv) - 1))
 
     for i, arg in enumerate(argv[1:], start=1):
         print('{:d}: {:s}'.format(i, arg))
+
+
