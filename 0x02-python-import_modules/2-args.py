a8346722e9648b21ebcd0ae84af7cfbee6012699
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-01-11 05:42:41 -0800

    fixed pycodestyles

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,16 +3,12 @@
 if __name__ == "__main__":
     from sys import argv
 
-
     if len(argv) == 1:
-
         print("0 arguments.")
     elif len(argv) == 2:
         print('{:d} argument:'.format(len(argv) - 1))
     else:
-        print ('{:d} argments:'.format(len(argv) - 1))
+        print('{:d} argments:'.format(len(argv) - 1))
 
-        for i, arg in enumerate(argv[1:], start=1):
-            print('{:d}: {:s}'.format(i, arg))
-
-
+    for i, arg in enumerate(argv[1:], start=1):
+        print('{:d}: {:s}'.format(i, arg))
