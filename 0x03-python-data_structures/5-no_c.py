cf0828bc959a87048d862b2638f629dba7c9acd4
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-01-13 07:35:05 -0800

    5-main.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_list = list(my_string)
+    new_str = ''
+    for i in new_list:
+        if i != 'c' and i != 'C':
+            new_str += i
+    return new_str
+
+
