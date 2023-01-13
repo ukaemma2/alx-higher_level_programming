6a83351c142c41201cd52911bd1ab7464b0e22ab
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-01-13 03:09:34 -0800

    fixed error

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return 'None'
-    elif idx > my_list[4]:
+    elif idx > len(my_list):
         return 'None'
     else:
-        return my_list[3]
+        return my_list[idx]
