74f93903839ecd00b8f814e35ff34700f648f5b6
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-01-13 13:44:27 -0800

    fixed bugs

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for cols in row:
-            print('{:d}'.format(cols), end=' ')
+    if len(matrix[0]) != 0:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        for row in range(rows):
+            for col in range(cols):
+                if col == cols - 1:
+                    print('{:d}'.format(matrix[row][col]))
+                else:
+                    print('{:d}'.format(matrix[row][col]), end=' ')
+    else:
         print()
