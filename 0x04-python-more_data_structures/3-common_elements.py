4d742641590ae566df612bd177b588fcd8b23420
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-01-15 13:42:42 -0800

    fixed bugs

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,10 +2,4 @@
 
 def common_elements(set_1, set_2):
     if set_1 is not None and set_2 is not None:
-
-        set_new_1 = set(set_1)
-        set_new_2 = set(set_2)
-
-        if (set_new_1 & set_new_2):
-            total = set_new_1 & set_new_2
-        return total
+        return set_1 & set_2
