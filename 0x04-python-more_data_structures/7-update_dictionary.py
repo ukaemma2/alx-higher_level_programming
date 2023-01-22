92975e1c7a3f865e564dc82bf3ecbf5fb165791d
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-01-22 08:14:55 -0800

    fixed bugs

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary  is not  None:
+    if a_dictionary is not  None:
         new_dic = a_dictionary
         new_dic[key] = value
         return new_dic
