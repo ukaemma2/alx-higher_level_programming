2682fd1533ec2d17587a5a0931563273fe706149
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-01-28 04:35:08 -0800

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,35 @@
+""" Square module definition
+This module defines a simple `Square` class
+"""
+
+
+class Square:
+
+    """A simple ``Square`` class
+    Attribute:
+        size (`int`): The size of the ``Square``.
+    """
+
+    def __init__(self, size=0):
+        """Construct a ``Square`` Object.
+        Args:
+            size (`int`): The size of the ``Square``.
+        Raises:
+            TypeError: if ``size`` is not an integer
+            ValueError: if ``size`` < 0
+
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+
+            self._Square__size = size
+
+    def area(self):
+        """compute the area of the ``Square``.
+        Return: 
+            int: the area of ``Square``.
+        """
+        return self._Square__size**2
