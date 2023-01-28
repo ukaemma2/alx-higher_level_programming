ece27afacf8f0d14a082981f996968f31473fbbf
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-01-28 03:12:02 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Square module definition
+This module defines a simple `Square` class
+"""
+
+
+class Square:
+    """A simple ``Square`` class
+    Attribute:
+        size (`int`): The size of the ``Square``.
+    """
+    def __int__(self, size):
+        """Construct a ``Square`` Object.
+        Args:
+            size (`int`): The size of the ``Square``.
+        """
+        self._Square__size =  size
