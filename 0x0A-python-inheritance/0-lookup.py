db61fc4bb4f74c3ac648d31c9275d617cc4e5c9d
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-02-07 07:23:10 -0800

    fixed pycode_errors

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 """Python Inheritance Modules"""
+
 
 def lookup(obj):
     """ function that returns the list of available.
     attributes and methods of an object
     """
-    return  (dir(obj))
+    return (dir(obj))
