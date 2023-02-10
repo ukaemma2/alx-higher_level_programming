7161eecde3de8487824bf3f455e45699dc12d6c1
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-02-10 11:46:51 -0800

    fixed pycodestyle error

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" function that writes a string to a text file (UTF8) and returns the number of characters written"""
+""" function that writes a string to a text file (UTF8)
+and returns the number of characters written
+"""
 
 
 def write_file(filename="", text=""):
