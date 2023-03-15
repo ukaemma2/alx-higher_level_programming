182870a20c6c06e74301b791df8d5e86fd9aff00
Ukah Emmanuel Ogbonna <ukaemmanuel2000@gmail.com>
2023-03-15 21:39:44 +0000

    comment_added

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
-reate a table with default values
+-- reate a table with default values
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES ('1', "John", '10');
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES ('2', "Alex", '3');
