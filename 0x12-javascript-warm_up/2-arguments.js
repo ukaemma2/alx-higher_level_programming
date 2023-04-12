1622eed03a9362cf0027573836dc3b8555045243
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-04-12 00:32:16 +0000

    more change

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-
-
 if (process.argv.lenght === 2) {
 	console.log("No argument");
 } else if (process.argv.length === 3) {
 	console.log("Argument found");
-}else {
+} else {
 	console.log("Argument found");
 }
