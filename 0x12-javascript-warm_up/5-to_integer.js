5a90fbee6421f4f423197d7689978ae6c82f3327
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-04-13 14:53:24 +0000

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 const args = process.argv[2];
-
-if (!isNaN(args) && parseInt(args) === args) {
+if (!isNaN(args) || parseInt(args) === args) {
   console.log(`My number: ${parseInt(args)}`);
 } else {
   console.log('Not a number');
