182d3b09043ad4ee914fb2c83b49731c238f3b3c
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-04-13 16:49:11 +0000

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2); 
+/* Extracting the arguments passed to the script */
+
+const x = parseInt(args[0])
+/* Parse first argument as integer */
+ let i = 0;
+
+if (isNaN(x)) {
+  console.log('Mission number of occurrences');
+  /* print error if the argument is not a number */
+} else {
+	for (; i < x; i++) {
+	  console.log('C is fun');
+		/* print C is fun */
+	}
+}
+
