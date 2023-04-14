ae2de9425a9e07c09e732c4662f968e816f0c593
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-04-14 06:59:34 +0000

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+const input = parseInt(process.argv[2]);
+console.log(`Input: ${input}`);
+console.log(`factorial: ${factorial(input)}`);
