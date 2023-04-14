3de68a533a327c3539297c4eea4dd6fc4b9507be
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-04-14 20:03:14 +0000

    101-main.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
-exports.callMeMoby = function(x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
   let i = 0;
-  
   for (; i < x; i++) {
     theFunction();
   }
-}
+};
