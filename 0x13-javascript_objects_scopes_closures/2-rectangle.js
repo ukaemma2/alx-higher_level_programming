4f879e5f2d69eea4df979f06abdcf0c6c8eaa549
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-04-14 23:01:10 +0000

    1-main.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+class Rectangle {
+  if ((w > 0) && (h > 0)) {
+    constructor (w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle;
