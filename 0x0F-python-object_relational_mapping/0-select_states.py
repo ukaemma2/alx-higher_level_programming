4324ef8fcfeb0969ab651bcc198ce447455f4086
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-04-16 18:54:48 +0000

    0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+"""Get username, password and database name from command line arguments"""
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    """Connect to MySQL server running on localhost at port 3306"""
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        password=password,
+        db=database
+    )
+
+    """ Create a cursor 0bject"""
+    cursor = db.cursor()
+
+    """Execute SQL query to retrieve all states from the database, sorted
+    by states.id """
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    """Fetch all the rows"""
+    rows = cursor.fetchall()
+
+    """Print the results"""
+    for row in rows:
+        print(row)
+
+    """ Close the cursor and database connections"""
+    cursor.close()
+    db.close()
