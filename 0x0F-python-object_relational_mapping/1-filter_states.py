b78e136493a318438a97d68ac777e1027ca79122
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-04-16 23:51:59 +0000

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     """Build the SQL Query"""
-    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    query = """SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"""
 
     """execute the query"""
     cursor.execute(query)
