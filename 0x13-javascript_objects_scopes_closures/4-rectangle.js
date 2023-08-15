27baef5c9c9cdd0c8e6ef84fab7c275187be9c5e
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-08-15 15:01:09 +0000

    rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+class Rectangle {
+	constructor (w,h) {
+		if ((w > 0) && (h > 0)) {
+			this.width = w;
+			this.height = h;
+		}
+	}
+
+	print () {
+		for (let i = 0; i < this.height; i++) {
+			let result = '';
+			for (let j = 0; j < this.width; j++) {
+				result += 'X';
+			}
+			console.log(result)
+		}
+	}
+	rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
