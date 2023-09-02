80a5ecb745275b945f3d54ef3c50c93f617af99e
ukaemma2 <ukaemmanuel2000@gmail.com>
2023-09-02 11:39:15 +0000

    removed bugs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,13 @@
-#!/usr/bin/env python3
-
-# A Python script that fetches https://alx-intranet.hbtn.io/status
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
 
-try:
-    with urllib.request.urlopen(url) as res:
-        content = res.read()
-
-    print("Body respond:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
-    print("\t- utf-8 content: {}".format(content.decode('utf-8')))
-
-except urllib.error.URLError as e:
-    print("Error: {}".format(e))
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
